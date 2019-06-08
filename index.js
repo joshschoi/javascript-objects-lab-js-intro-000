fbf994d6c974db45613c2e6a4dc965a3b554ed0b
Joshua Choi <jsc.r839@gmail.com>
2019-06-08 21:00:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newobject = object;
+  var newObj = Object.assign({}, object)
   delete newobject[key];
   return newobject;
 }
